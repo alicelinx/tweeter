d29af4b3ff5250c457e7ce2391bc7464aac185c0
Alice Lin <alicexxlin@gmail.com>
2023-06-21 20:20:39 +0000

    implement dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,52 +3,70 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
 
-  let tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+$(() => {  // more modern than $(document).ready(function() {
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
     },
-    "created_at": 1461116232227
-  };
+  ];
 
-  const createTweetElement = function(obj) {
+  const createTweetElement = function(tweet) {
 
-    const markup = `
-      <section class="tweet-container">
-        <article class="tweet">
-          <header>
-            <div class="photo-name">
-              <img src="${obj.user.avatars}">
-              <a class="user-name">&#160&#160${obj.user.name}</a>
-            </div>
-            <a class="user-id">${obj.user.handle}</a>
-          </header>
-          <div>
-            <a class="tweet-content">${obj.content.text}</a>
+    let $tweet = `
+      <article class="tweet">
+        <header>
+          <div class="photo-name">
+            <img src="${tweet.user.avatars}">
+            <a class="user-name">&#160&#160${tweet.user.name}</a>
           </div>
-          <footer>
-            <a>${obj.created_at}</a>
-            <a><i class="fa-solid fa-flag"></i>&#160&#160<i class="fa-solid fa-retweet"></i>&#160&#160<i class="fa-solid fa-heart"></i></a>
-          </footer>
-        </article>
-      </section>
-      `;
+          <a class="user-id">${tweet.user.handle}</a>
+        </header>
+          <div>
+            <a class="tweet-content">${tweet.content.text}</a>
+          </div>
+        <footer>
+          <a>${tweet.created_at}</a>
+          <a><i class="fa-solid fa-flag"></i>&#160&#160<i class="fa-solid fa-retweet"></i>&#160&#160<i class="fa-solid fa-heart"></i></a>
+        </footer>
+      </article>
+    `;
 
-    return markup;
+    // takes return value and appends it to the tweets container
+    $('.tweet-container').append($tweet);
+
   };
 
-  // document.body.innerHTML = markup;
+  const renderTweets = function(tweets) {
 
-  const $tweet = createTweetElement(tweetData);
+    // loops through tweets
+    for (let tweet of tweets) {
+      // calls createTweetElement for each tweet
+      createTweetElement(tweet);
+    }
+  };
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 
 });
